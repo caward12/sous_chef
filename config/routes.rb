4885cd26258930b78cba9c386b314378a1835267
caward12 <ward.colleen.a@gmail.com>
2017-07-20 18:52:39 -0600

    add user api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get "/", to: "home#index"
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :update, :show]
+    end
+  end
 end
