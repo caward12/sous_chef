7fba22d8278a664ba075ae325c28b8bf5a86baef
caward12 <ward.colleen.a@gmail.com>
2017-07-25 20:18:07 -0600

    can get ingredients from form

--- app/assets/javascripts/create_recipe.js
+++ app/assets/javascripts/create_recipe.js
@@ -2,21 +2,40 @@ $(document).ready(function(){
   $('.create-ingredients').hide()
   bindAddIngredientswithForm()
   bindAddInstructionswithFrom()
+  bindAddAnotherIngredientwithPartial()
 })
 
+function bindAddAnotherIngredientwithPartial(){
+  $('.create-ingredients #addNewIngredient').on("click", function(event){
+    $(".ingredients").append($("#new_ingredients_form").html())
+  })
+}
+
 function bindAddInstructionswithFrom(){
-  $('.create-recipe .add-instructions').on("click", function(event){
+  $('.create-ingredients .add-instructions').on("click", function(event){
     event.preventDefault()
+    // getIngredients()
     createIngredients()
   })
 }
 
+function getIngredients(){
+return
+}
+
 function createIngredients(){
+  var arr = []
+  Array.from($('.ingredients .ingredient-fields')).forEach(function(row){
+    var data =  {}
+       data["name"]= $(row).children()[0].children[0].value,
+       data["amount"]= $(row).children()[1].children[0].value
+       arr.push(data)
+            })
   var ingredients = {
-    ingredients: {
-      
-    }
+    ingredients: {arr}
   }
+
+
 }
 function bindAddIngredientswithForm(){
   $('.create-recipe .add-ingredients').on("click", function(event){
@@ -37,10 +56,10 @@ function createRecipe(){
   $.post("/api/v1/recipes",recipeData)
         .then(function(newRecipe) {
             $('.create-recipe').hide()
-            $('.create-ingredients').data("recipe", newRecipe.id)
             if ($('.create-ingredients').is(":hidden")){
               $('.create-ingredients').show()
             }
+            $('.create-ingredients').attr('id', `${newRecipe.id}`)
           })
 
 
