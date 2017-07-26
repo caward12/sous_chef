812025ee6c68c28a2e3713a4f4ff6a05bb164292
caward12 <ward.colleen.a@gmail.com>
2017-07-25 21:22:55 -0600

    add instructions working

--- app/assets/javascripts/create_recipe.js
+++ app/assets/javascripts/create_recipe.js
@@ -1,9 +1,36 @@
 $(document).ready(function(){
   $('.create-ingredients').hide()
+  $('.create-instructions').hide()
+  $('.create-categories').hide()
   bindAddIngredientswithForm()
-  bindAddInstructionswithFrom()
+  bindAddInstructionswithForm()
+  bindAddCategorieswithForm()
   bindAddAnotherIngredientwithPartial()
 })
+
+function bindAddCategorieswithForm(){
+  $('.create-instructions .add-categories').on("click", function(event){
+    event.preventDefault()
+    createInstructions()
+  })
+}
+
+function createInstructions(){
+  var recipe = $('.create-instructions').attr('id')
+  var instructions = {recipe: {instructions: $('textarea#instructions').val()}}
+
+  $.ajax({
+    type: "PUT",
+    url: `/api/v1/recipes/${recipe}`,
+    data: instructions
+  }).then(function(updatedRecipe){
+    $('.create-instructions').hide()
+    if ($('.create-categories').is(":hidden")){
+      $('.create-categories').show()
+    }
+    $('.create-categories').attr('id', `${updatedRecipe.id}`)
+  })
+}
 
 function bindAddAnotherIngredientwithPartial(){
   $('.create-ingredients #addNewIngredient').on("click", function(event){
@@ -11,31 +38,38 @@ function bindAddAnotherIngredientwithPartial(){
   })
 }
 
-function bindAddInstructionswithFrom(){
+function bindAddInstructionswithForm(){
   $('.create-ingredients .add-instructions').on("click", function(event){
     event.preventDefault()
-    // getIngredients()
+
     createIngredients()
   })
 }
 
-function getIngredients(){
-return
-}
-
 function createIngredients(){
+  var recipe = $('.create-ingredients').attr('id')
   var arr = []
   Array.from($('.ingredients .ingredient-fields')).forEach(function(row){
     var data =  {}
        data["name"]= $(row).children()[0].children[0].value,
        data["amount"]= $(row).children()[1].children[0].value
        arr.push(data)
-            })
+  })
   var ingredients = {
-    ingredients: {arr}
+    recipe_id: recipe,
+    ingredients: arr
   }
-
-
+  $.ajax({
+    type: "POST",
+    url: "/api/v1/ingredients",
+    data: ingredients
+  }).then(function(newIngredients){
+    $('.create-ingredients').hide()
+    if ($('.create-instructions').is(":hidden")){
+      $('.create-instructions').show()
+    }
+    $('.create-instructions').attr('id', `${newIngredients[0].recipe_id}`)
+  })
 }
 function bindAddIngredientswithForm(){
   $('.create-recipe .add-ingredients').on("click", function(event){
